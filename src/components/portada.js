8cb475fb68fa9d00a37b93eb85e437b3669fa7d8
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2023-02-03 00:12:23 +0100

    visual

--- src/components/portada.js
+++ src/components/portada.js
@@ -3,7 +3,7 @@ import disco from "../components/portadaDisco.jpg"
 import estiloportada from "../components/portada.css"
 function Portada() {
 return (
-    <div style={{"text-align": "center", height: '80vh'}}>
+    <div style={{"text-align": "center", height: '60vh'}}>
         <a href='https://warnermusicspain.lnk.to/RecycledJ_150CANCIONES' target="_blank">
             <img src={disco} style={{width: "80%"}} /></a>
 </div>
