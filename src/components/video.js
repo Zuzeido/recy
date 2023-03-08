c74c534ce1ac0969e3d36916b1c15a268c586933
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2023-03-08 23:34:00 +0100

    added video
    add videos

--- src/components/video.js
+++ src/components/video.js
@@ -3,19 +3,81 @@ import YouTube, { YouTubeProps } from 'react-youtube';
 
 
 function video() {
-    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-      }
-    
-      const opts: YouTubeProps['opts'] = {
-        height: '700',
-        width: '95%',
-        playerVars: {
-          autoplay: 1,
-        },
-      };
-    
-      return <YouTube videoId="V4AAOJfnVY0" opts={opts} onReady={onPlayerReady} className="video" style={{"display": "flex","flex-wrap": "nowrap","justify-content": "center", marginTop: "3rem"}}/>;
-    }
+  return (
+    <>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/V4AAOJfnVY0?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/F6AFhjVl8OI?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/tpBPFigHlBE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/xrvHHU2cR8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/r2wYsJ-Y710" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    <div style={{
+      marginTop: "3rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/nPHvYyB-K1E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    </>
+    )
+}
 export default video
