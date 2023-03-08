032c61dddb2d22cfc4ff0478f8a8c32f931ea38d
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2023-03-08 23:54:40 +0100

    update video home
    home video added

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,7 +2,23 @@ import React from "react";
 import Rose from "./rose/Rose"
 function Inicio() {
   return (
-     <Rose />
+    <>     
+    <Rose />
+     <div style={{
+      marginTop: "0.2rem",
+      marginBottom: "2rem",
+      display: "flex",
+      "flex-wrap": "nowrap",
+      "justify-content": "center"
+      }}>
+    <iframe 
+    width="90%" 
+    height="315" 
+    src="https://www.youtube.com/embed/V4AAOJfnVY0?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+    </iframe>
+    </div>
+    </>
+
   );
 }
 
