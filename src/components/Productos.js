45ccb29c882467ba74278de5fb99604f9a7adc77
ruben.rodriguezinigo@cuatrodigital.com <ruben.rodriguezinigo@cuatrodigital.com>
2023-03-13 20:28:48 +0100

    add images

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -31,32 +31,24 @@ import "./products.css"
 const Index = () => {
 
     const [users, setUsers] = useState([]);
-
-
-
     const getProducts = async () => {
-        const response = await fetch("https://recycledj.com/wp-json/wc/v3/products/?per_page=50&consumer_key=ck_00b2f37c17aa393706939ecb185e4c88d0c16773&consumer_secret=cs_f7c56d656d32ebcd3cf787492189eae8f44a3f91&stock_status=instock&status=publish");
+        const response = await fetch("https://recycledj.com/wp-json/wc/v3/products/?per_page=50&consumer_key=ck_00b2f37c17aa393706939ecb185e4c88d0c16773&consumer_secret=cs_f7c56d656d32ebcd3cf787492189eae8f44a3f91");
         const FinalData = await response.json();
         setUsers(FinalData)
     }
-
-
-
     useEffect(() => {
         getProducts();
     }, [])
-
-
+    
     return (
-      
-            <div className="container">
-               
+            <div className="container">              
                 {
                     users.map((curElem) => {
                         return (
 
                             <div className="card_item" key={curElem.id}>
                                 <div className="card_inner">
+                                    <img src={curElem.images[0].src} alt="" />
                                     <div className="userName">{curElem.name}</div>
                                     <div className="userUrl">{curElem.id}</div>
                                     <div className="price">{curElem.price}€</div>
@@ -67,9 +59,7 @@ const Index = () => {
                         )
                     })
                 }
-
             </div>              
-
     )
 }
 
